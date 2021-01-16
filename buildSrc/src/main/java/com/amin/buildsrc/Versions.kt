95f42947b23eca9883be77059a62136ef822a945
Amin Moradian <amin.moradian@protonmail.com>
2021-01-16 11:56:03 +0330

    settings buildSrc

---
+++ buildSrc/src/main/java/com/amin/buildsrc/Versions.kt
@@ -0,0 +1,9 @@
+package com.amin.buildsrc
+
+object Versions {
+  const val compileSdkVersion = 30
+  const val minSdkVersion = 21
+  const val targetSdkVersion = 30
+  const val versionCode = 1
+  const val versionName = "1.0"
+}
